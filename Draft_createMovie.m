bac549cc205d37e5d6a5ef3dd4af143033b8e23c
anapdinizm <anapdinizm>
2014-05-22 20:24:33 -0300

    Update Draft_creatMovie.m

--- Draft_createMovie.m
+++ Draft_createMovie.m
@@ -1,7 +1,7 @@
-%Making video file .avi, .mp4
+%Making video file .avi
 %
 %Example:
-% writerObj = VideoWriter('peaks.avi');
+% writerObj = VideoWriter('C:\Users\Convidado.Benedito-HP\Desktop\peaks.avi');
 % open(writerObj);
 % 
 % Z = peaks; surf(Z); 
@@ -9,7 +9,7 @@
 % set(gca,'nextplot','replacechildren');
 % set(gcf,'Renderer','zbuffer');
 % 
-% for k = 1:20 
+% for k = 1:200 
 %    surf(sin(2*pi*k/20)*Z,Z)
 %    frame = getframe;
 %    writeVideo(writerObj,frame);
@@ -19,11 +19,11 @@
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Arg in:
-%Str= 'name.mp4' file name;
+%Str= 'C:\Users\John\Desktop\name' file name;
 %tf = final time;
 %frame = variable it will be show in the movie
-function createMovie(str,tf,frame)
-
+function Draft_createMovie(str,tf,frame)
+%cd file
 writerObj = VideoWriter(str);
 open(writerObj);
 
